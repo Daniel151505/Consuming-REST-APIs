3be66e900675261644c1961495d1e02f8c87f03f
Daniel151505 <daniel150520011@gmail.com>
2022-01-03 21:06:33 -0500

    Use of interceptors

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SwiperModule } from 'swiper/angular';
 import SwiperCore, { Pagination } from "swiper";
 
@@ -14,6 +14,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
+import { TimeInterceptor } from './interceptors/time.interceptor';
 
 SwiperCore.use([Pagination]);
 @NgModule({
@@ -35,7 +36,9 @@ SwiperCore.use([Pagination]);
     SwiperModule,
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
